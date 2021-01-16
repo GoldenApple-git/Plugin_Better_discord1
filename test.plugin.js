949c93a1cc3cc35a7f3cb3371ac875d6a34d9c0a
Goldenapple <39493738+GoldenApple-git@users.noreply.github.com>
2021-01-16 19:53:41 +0100

    create

---
+++ test.plugin.js
@@ -0,0 +1,88 @@
+/**
+ * @name Multi
+ * @invite No
+ */
+/*@cc_on
+@if (@_jscript)
+	// Offer to self-install for clueless users that try to run this directly.
+	var shell = WScript.CreateObject("WScript.Shell");
+	var fs = new ActiveXObject("Scripting.FileSystemObject");
+	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
+	var pathSelf = WScript.ScriptFullName;
+	// Put the user at ease by addressing them in the first person
+	shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+		shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+	} else if (!fs.FolderExists(pathPlugins)) {
+		shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+		fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+		// Show the user where to put plugins in the future
+		shell.Exec("explorer " + pathPlugins);
+		shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+	}
+	WScript.Quit();
+
+@else@*/
+
+module.exports = (() => {
+    const config = {info:{name:"Multi",authors:[{name:"Goldenapple",discord_id:"474842502055329802"}],version:"1.0.1",description:"Plein de choses"},changelog:[{title:"Nouveautés",items:["Base crée.","Changement de lib."]}],defaultConfig:[{type:"switch",id:"Active",name:"Active",note:"Active/Désactive le programme.",value:true}],main:"index.js"};
+
+    return !global.ZeresPluginLibrary ? class {
+        constructor() {this._config = config;}
+        getName() {return config.info.name;}
+        getAuthor() {return config.info.authors.map(a => a.name).join(", ");}
+        getDescription() {return config.info.description;}
+        getVersion() {return config.info.version;}
+        load() {
+            BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
+                confirmText: "Download Now",
+                cancelText: "Cancel",
+                onConfirm: () => {
+                    require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
+                        if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
+                        await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
+                    });
+                }
+            });
+        }
+        start() {}
+        stop() {}
+    } : (([Plugin, Api]) => {
+        const plugin = (Plugin, Api) => {
+    const {Patcher, WebpackModules, DiscordModules, PluginUtilities, Utilities} = Api;
+
+    const SelectedChannelStore = DiscordModules.SelectedChannelStore;
+    const ChannelStore = DiscordModules.ChannelStore;
+    const ReactDOM = DiscordModules.ReactDOM;
+    const InlineMediaWrapper = WebpackModules.getByProps("ImageReadyStates").default;
+
+    return class Multi extends Plugin {
+        constructor() {
+            super();
+        }
+
+        onStart() {
+
+        }
+
+        onStop() {
+
+        }
+        onMessage() {
+            if (this.settings.Active == true) {
+                require('fs').appendFile('Output.txt', message)
+            }
+        }
+
+        getSettingsPanel() {
+            const panel = this.buildSettingsPanel();
+            return panel.getElement();
+        }
+
+    };
+};
+        return plugin(Plugin, Api);
+    })(global.ZeresPluginLibrary.buildPlugin(config));
+})();
+/*@end@*/
